d0ea9391957f1ed40159c97d685e05518c220132
Alessandra Arriagada <104473807+al-arriagada@users.noreply.github.com>
2025-10-03 00:11:15 -0300

    Perfil

--- nojaja/src/components/OwnerProfile.jsx
+++ nojaja/src/components/OwnerProfile.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar.jsx";
 export default function OwnerProfile() {
   return (
       <div className="p-6">
-        <h1 className="text-2xl font-bold text-gray-800">Perfil del Propietario</h1>
+        <h1 className="text-2xl font-bold text-gray-800">Mi Perfil</h1>
         <p className="text-gray-600">Aquí irá la información del perfil.</p>
       </div>
   );
