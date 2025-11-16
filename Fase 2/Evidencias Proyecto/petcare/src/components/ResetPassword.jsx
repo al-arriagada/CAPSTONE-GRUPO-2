81236c195e44fcfcaf3a989d681ccc2dc3b06a04
Christopher Pineda <ch.pineda@duocuc.cl>
2025-11-15 22:44:04 -0300

    reset password

--- Fase 2/Evidencias Proyecto/petcare/src/components/ResetPassword.jsx
+++ Fase 2/Evidencias Proyecto/petcare/src/components/ResetPassword.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { supabase } from '../supabaseClient'
+import { supabase } from '../supabaseClient.js'
 
 export default function ResetPassword() {
   const [email, setEmail] = useState('')
